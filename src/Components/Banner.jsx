0cd111fa9413189d173ba515ec70421807e9bde7
seblacour87 <seblacour87@gmail.com>
2026-06-24 15:21:07 +0200

    page home responsive ok

--- src/Components/Banner.jsx
+++ src/Components/Banner.jsx
@@ -3,9 +3,15 @@ import bannerImage from "../Assets/images/4706e7dde270fc8fc902a1eb738458e7b29c18
 const Banner = () => {
   return (
     <div className="banner">
-      <img src={bannerImage} alt="photo d'une côte rocheuse" />
-      <div className="banner-overlay"></div>
-      <h1>Chez vous, partout et ailleurs</h1>
+      <div className="banner-clip">
+        <img src={bannerImage} alt="photo d'une côte rocheuse" />
+        <div className="banner-overlay">
+          <h1>
+            Chez vous,
+            <br /> partout et ailleurs
+          </h1>
+        </div>
+      </div>
     </div>
   );
 };
