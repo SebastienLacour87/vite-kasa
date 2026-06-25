079440c1ecb8ee22638f6821a8c1919ed4926c62
seblacour87 <seblacour87@gmail.com>
2026-06-25 11:53:39 +0200

    page About version desktop ok, version responsive en cours

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,11 +1,49 @@
-import React from "react";
 import Header from "../Components/Header";
 import Footer from "../Components/Footer";
+import Banner from "../Components/Banner";
+import bannerImage from "../Assets/images/56fae17eb9995860bb6384a77ca7dc9bf52da3be.jpg";
+import Collapse from "../Components/Collapse";
 
 const About = () => {
+  const sections = [
+    {
+      title: "Fiabilité",
+      content:
+        "Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées  par nos équipes.",
+    },
+    {
+      title: "Respect",
+      content:
+        "La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme. ",
+    },
+    {
+      title: "Service",
+      content:
+        "La qualité du service est au cœur de notre engagement chez Kasa. Nous veillons à ce que chaque interaction, que ce soit avec nos hôtes ou nos locataires, soit empreinte de respect et de bienveillance.",
+    },
+    {
+      title: "Sécurité",
+      content:
+        "La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes.",
+    },
+  ];
   return (
     <div>
       <Header />
+      <Banner
+        image={bannerImage}
+        alt="Photo d'un paysage montagneux"
+        overlayOpacity={0.3}
+      />
+      <div className="section-collapse">
+        {sections.map((section) => (
+          <Collapse
+            key={section.title}
+            title={section.title}
+            content={section.content}
+          />
+        ))}
+      </div>
       <Footer />
     </div>
   );
